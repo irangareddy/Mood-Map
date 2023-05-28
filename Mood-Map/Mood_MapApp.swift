af685531c82a6af8438b099e51967e27615185bb
Ranga Reddy Nukala <itsmerr17@gmail.com>
2023-05-28 11:57:49 +0530

    project initialisation

---
+++ Mood-Map/Mood_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_MapApp.swift
+//  Mood-Map
+//
+//  Created by Ranga Reddy Nukala on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Mood_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
