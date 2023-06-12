aa8118a252613dd20379a052a4a6a1deb568dfa5
Ranga Reddy Nukala <rangareddy.nukala@eccenca.com>
2023-06-12 23:00:32 +0530

    add final app icon for mood map

---
+++ Mood-Map/Onboarding/Onboarding.swift
@@ -0,0 +1,8 @@
+//
+//  Onboarding.swift
+//  Mood-Map
+//
+//  Created by Ranga Reddy Nukala on 12/06/23.
+//
+
+import Foundation
---
+++ Mood-Map/Onboarding/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Mood-Map
+//
+//  Created by Ranga Reddy Nukala on 12/06/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
